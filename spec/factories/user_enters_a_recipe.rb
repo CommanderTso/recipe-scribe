a35575b72d92373c33da7afb0c2cc7686c48524a
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-17 19:51:24 -0400

    Initial commit

---
+++ spec/factories/user_enters_a_recipe.rb
@@ -0,0 +1,13 @@
+require 'rails-helper'
+
+# As a user, I want to input recipes.
+#
+# **Acceptance Criteria:**
+# - User can input a recipe with:
+# -- a title (required)
+# -- one or more ingredients (required)
+# -- instructions (optional)
+# - User is messaged if the entry succeeds
+# - User is messaged with informative errors if the entry fails.
+
+feature 
