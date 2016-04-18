22f36a491011ce676eb2c62d23799bf538d002dc
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-18 17:58:18 -0400

    Factory and test for (already implemented) viewing of recipe index

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :recipe do
+    title "My Old School Baked Ziti"
+    instructions %(A few notes: To make this without meat, as I’m not
+      personally into meat substitutes, I would use a pound or so of sliced
+      mushrooms instead to make this vegetarian.)
+  end
+end
