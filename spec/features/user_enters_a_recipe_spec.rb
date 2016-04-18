91b725cc615b7be4c95877bf7640865801ed2da4
Scott Macmillan <scottmacmillan@gmail.com>
2016-04-18 16:03:17 -0400

    Styling & Enter / View Recipe
    - More moves to add/adjust styling with Foundation and Sass
    - Add a title to the site
    - Working form for recipe submission
    - Delete a lot of boilerplate Rails comments
    - Create some seeds.rb data
    - First two user stories complete and passing
    - Revise Recipes table to have a :title instead of a :name
    - Add some basic validations to recipe.rb

--- spec/features/user_enters_a_recipe_spec.rb
+++ spec/features/user_enters_a_recipe_spec.rb
@@ -10,9 +10,32 @@ require 'rails_helper'
 # - User is messaged if the entry succeeds
 # - User is messaged with informative errors if the entry fails.
 
-feature "User inputs a recipe" do
 
-  scenario "placeholder tests to make sure CI works" do
-    expect(true).to be_truthy
+
+feature "User inputs a recipe" do
+  scenario "User inputs a recipe" do
+    visit root_path
+    page.find('#add_recipe').click
+
+    expect(page).to have_content "Add a new recipe here:"
+
+    fill_in "Title", with: "Title of a Recipe"
+    fill_in "Instructions", with: "Instructions on how to make it!"
+
+    click_button "Add Recipe"
+
+    expect(page).to have_content("Title of a Recipe")
+    expect(page).to have_content("Your recipe has been saved!")
+  end
+
+  scenario "User leaves out recipe title" do
+    visit root_path
+    page.find('#add_recipe').click
+
+    click_button "Add Recipe"
+
+    expect(page).to have_content("Add a new recipe here:")
+    expect(page).to have_content("Title can't be blank")
+
   end
 end
